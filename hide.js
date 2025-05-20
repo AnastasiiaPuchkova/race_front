fb9c9fb4beccf05c681152e0a24aea5b405ea288
dkrychun <dkrychun@stud.khpi.ucode-connect.study>
2025-05-20 20:26:33 +0300

    im sorry

---
+++ hide.js
@@ -0,0 +1,4 @@
+document.querySelector('.btn.play').addEventListener('click', () => {
+    document.querySelector('.menu-box').style.display = 'none';
+    document.querySelector('.game_starting').style.display = 'block';
+  });
